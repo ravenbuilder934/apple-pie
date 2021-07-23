05408136890e408a6498acb1f5658fd968353ba1
Raven <sabrinajamie@outlook.com>
2021-07-23 12:48:45 -0500

    Update to 1.17.1

--- src/main/java/applepie/item/FoodRegistry.java
+++ src/main/java/applepie/item/FoodRegistry.java
@@ -1,8 +1,8 @@
 package applepie.item;
 
-import net.minecraft.item.Food;
+import net.minecraft.world.food.FoodProperties;
 
 public class FoodRegistry
 {
-    public static final Food APPLEPIE = (new Food.Builder()).nutrition(8).saturationMod(4.0F).build();
+    public static final FoodProperties APPLEPIE = (new FoodProperties.Builder()).nutrition(8).saturationMod(4.0F).build();
 }
--- src/main/java/applepie/item/ItemRegistry.java
+++ src/main/java/applepie/item/ItemRegistry.java
@@ -1,9 +1,9 @@
 package applepie.item;
 
 import applepie.ApplePie;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraftforge.fml.RegistryObject;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -11,5 +11,5 @@ public class ItemRegistry
 {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ApplePie.MOD_ID);
 
-    public static final RegistryObject<Item> applepie = ITEMS.register("applepie", () -> new Item((new Item.Properties()).tab(ItemGroup.TAB_FOOD).food(FoodRegistry.APPLEPIE)));
+    public static final RegistryObject<Item> applepie = ITEMS.register("applepie", () -> new Item((new Item.Properties()).tab(CreativeModeTab.TAB_FOOD).food(FoodRegistry.APPLEPIE)));
 }
